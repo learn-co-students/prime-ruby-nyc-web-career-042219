c9303b1becd04adce1fbfa963dc5d5ccd3adfa5d
Yasmine Hartung <y.hartung@gmail.com>
2019-03-05 09:38:20 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
-# Add  code here!
+def prime? (num)
+  divisors = 0
+  pot_divisors = (1..num).to_a
+  pot_divisors.each do |div|
+    remainder = num % div
+    if remainder == 0
+      divisors += 1
+    end
+  end
+  if divisors == 2
+    return true
+  else
+    return false
+  end
+
+end
