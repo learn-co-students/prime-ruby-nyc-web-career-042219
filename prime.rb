f40ed7a5100dadf123f24127210dd22b9fca267e
Jason Gomez <gmzjsn@gmail.com>
2019-03-24 00:53:30 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,7 +1,6 @@
 # Add  code here!
 def prime?(num)
-  num = num.abs
-  return false if num == 0 || num == 1
+  return false if num <= 1
   return true if num == 2 
   (2...num.abs).each do |i|
     return false if num % i == 0 
