fd176b77b7f2ebff7c8e3e5029f923099e116e15
Catherine Batsoula <cbatsoula@gmail.com>
2019-04-05 22:47:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(array)
-  (-1..9).to_a
+  #create a range of numbers and turn it into an array
+  #use booleans and conditionals to test each integer if it is prime
+  #prime numbers are only divisible by themselves and 1 
+  #   and not or
+  #
   
 
 end
