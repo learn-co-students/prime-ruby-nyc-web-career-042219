b199936e529b8c96f0c08ce23ef38d02a52e0ff8
github username <github email address>
2019-04-08 12:14:51 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,21 @@
 # Add  code here!
+def prime?(int)
+
+  if int < 0
+    int = int * -1
+  end
+
+  if int <= 1
+    return false
+  end
+
+  num_range =* (2...int)
+
+  num_range.each do |i|
+    if int % i == 0
+      return false
+    end
+  end
+  return true
+
+end
