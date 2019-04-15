3e0ec2656dcd0cc3fccfc1c4f0db6e3cff3c0daf
Christian <christianeduncan3@gmail.com>
2019-04-14 20:31:15 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(num)
+  # Add  code here!
+  if num < 0
+    num = num * -1
+end
+
+if num <= 1
+  return false
+end
+
+range =* (2...num)
+
+range.each do |i|
+  if num % i == 0
+    return false
+end
+end
+return true
+end
