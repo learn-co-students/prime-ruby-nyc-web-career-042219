73dcda42727d56f5a27b1dab65d19b30d7d09017
Jessica Lin <jessicaa.lin@gmail.com>
2019-04-15 17:34:58 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  if num < 2
+    return false
+  end
+  for divisor in 2..(num-1)
+    if (num % divisor) == 0
+      return false
+    end
+  end
+  return true
+end
