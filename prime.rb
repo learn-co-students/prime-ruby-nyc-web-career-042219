1c7d228c3eabc3134c003125fdc39e24e5ed7c88
Christian Pinto <pintochristian2@gmail.com>
2019-04-16 00:53:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1 
+    return false
+  elsif num == 2
+    return true
+  else
+    range = (2..num/2).to_a
+    range.none? {|i| num % i == 0}
+  end
+end
